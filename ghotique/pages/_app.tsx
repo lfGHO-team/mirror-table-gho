5566a7d5a71db362a529865ee1684492da61d438
shadowfaz16 <erikfazju@gmail.com>
2024-01-21 04:08:34 -0600

    one more try

--- ghotique/pages/_app.tsx
+++ ghotique/pages/_app.tsx
@@ -16,7 +16,7 @@ const config = createConfig(
     //infuraId: process.env.NEXT_PUBLIC_INFURA_ID,
     //alchemyId:  process.env.NEXT_PUBLIC_ALCHEMY_ID,
     chains: [sepolia, mainnet, polygon, optimism, arbitrum],
-    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
+    walletConnectProjectId: "e529e3d10c022978967188ab28b77b3f",
   })
 );
 
@@ -25,7 +25,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <WagmiConfig config={config}>
       <ThirdwebProvider
         activeChain={Sepolia}
-        clientId={process.env.NEXT_PUBLIC_PROJECT_ID as string}
+        clientId={"578c70b3c62e89569ef212bdf3261dd5"}
       >
         <ConnectKitProvider theme="midnight"
           customTheme={{
